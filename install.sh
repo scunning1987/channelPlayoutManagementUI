18a0b4bc0f60b0fb25df22a39ac645f2e41935e4
root <root@ip-172-31-7-143.us-west-2.compute.internal>
2020-04-24 22:06:59 +0000

    nginx dependencies

--- install.sh
+++ install.sh
@@ -28,8 +28,10 @@ yum -y remove nginx
 #wget http://nginx.org/download/nginx-1.14.0.tar.gz
 tar xvzf nginx-1.14.0.tar.gz
 cd nginx-1.14.0
-git clone https://github.com/nginx/njs.git
-git clone https://github.com/arut/nginx-rtmp-module.git
+#git clone https://github.com/nginx/njs.git
+#git clone https://github.com/arut/nginx-rtmp-module.git
+mv ../njs .
+mv ../nginx-rtmp-module .
 #
 # install nginx
 ./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib64/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches -m64 -mtune=generic -fPIC' --with-ld-opt='-Wl,-z,relro -Wl,-z,now -pie' --add-module=nginx-rtmp-module
