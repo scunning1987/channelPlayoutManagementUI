1d847cbecd51236f81fb70f47d0b537a3486b83b
root <root@ip-172-31-41-181.us-west-2.compute.internal>
2020-04-30 18:33:40 +0000

    hlsurl change in tmp folder

--- scripts/webpageurlset.sh
+++ scripts/webpageurlset.sh
@@ -1,5 +1,8 @@
 webpage="/etc/nginx/html/mcr/softpanel.html"
+javascript="/etc/nginx/html/mcr/js/pagescript.js"
+hlsURL="http://myserver33/myindex.m3u8"
 
+# RTMP URL change
 old=$(cat $webpage | grep -o rtmp.*:1935)
 new=rtmp:\\/\\/$(curl -s ifconfig.me):1935
 
@@ -10,8 +13,6 @@ for rtmp in ${rtmparray[@]}; do
   sed -i 's/'$oldslash'/'$new'/g' $webpage
 done
 
-javascript="/etc/nginx/html/js/pagescript.js"
-
 old=$(cat $javascript | grep -o rtmp.*:1935)
 new=rtmp:\\/\\/$(curl -s ifconfig.me):1935
 
@@ -19,5 +20,25 @@ new=rtmp:\\/\\/$(curl -s ifconfig.me):1935
 rtmparray=( $old )
 for rtmp in ${rtmparray[@]}; do
   oldslash=$(echo ${rtmp////\\/})
+  sed -i 's/'$oldslash'/'$new'/g' $javascript
+done
+
+# HLS index change
+old=$(cat $webpage | grep -o http.*m3u8)
+new=$(echo ${hlsURL////\\/})
+
+#array of hls entries
+hlsarray=( $old )
+for hls in ${hlsarray[@]}; do
+  oldslash=$(echo ${hls////\\/})
+  sed -i 's/'$oldslash'/'$new'/g' $webpage
+done
+
+old=$(cat $javascript | grep -o http.*m3u8)
+
+#array of hls entries
+hlsarray=( $old )
+for hls in ${hlsarray[@]}; do
+  oldslash=$(echo ${hls////\\/})
   sed -i 's/'$oldslash'/'$new'/g' $javascript
 done
